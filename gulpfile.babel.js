96291e736c4ba5f2e3923cdfa7e703d016bf2a67
DevelopJKong <ybaram40@gmail.com>
2021-12-18 23:25:36 +0900

    3.2 완료 gulp 문제 생길시 이곳으로 이동하기

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,9 @@ import gulpSass from "gulp-sass";
 import nodeSass from "node-sass";
 import autoprefixer from "gulp-autoprefixer";
 import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-bro";
+import babel from "babelify";
 const sass = gulpSass(nodeSass);
 
 const paths = {
@@ -11,7 +14,13 @@ const paths = {
     dest: "src/static/styles",
     watch: "assets/scss/**/*.scss",
   },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js",
+  },
 };
+const clean = () => del(["src/static"]);
 
 const styles = () => {
   return gulp
@@ -24,12 +33,27 @@ const styles = () => {
     )
     .pipe(minifyCSS())
     .pipe(gulp.dest(paths.styles.dest));
-}
+};
+
+const js = () => {
+  return gulp
+    .src(paths.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babel.configure({ presets: ["@babel/preset-env"] })
+     
+        ],
+      })
+    )
+    .pipe(gulp.dest(paths.js.dest));
+};
 
 const watchFiles = () => {
   gulp.watch(paths.styles.watch, styles);
-}
+  gulp.watch(paths.js.watch, js);
+};
 
-const dev = gulp.series([styles, watchFiles]);
+const dev = gulp.series([clean, styles, watchFiles, js]);
 
 export default dev;
