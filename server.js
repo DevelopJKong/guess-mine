7c9c5df8a8acc1904f983533536d50136ead4839
DevelopJKong <ybaram40@gmail.com>
2021-12-18 13:16:58 +0900

    2.3 완료 gulp 문제 해결 필요

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import { join } from "path";
 import express from "express";
-import socketIO from "socket.io"; 
+import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4050;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
   return res.render("home");
@@ -15,4 +17,19 @@ app.get("/", (req, res) => {
 const handleListening = () =>
   console.log(`😎 Server running: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    // object를 파라미터로 받네? 그럼 정확하게 어떻게 되는거지?
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
